e59ebbe823fcf9a948092469dfb6453e90e065f0
Tianjian Xia <xtjnj95@gmail.com>
2020-08-18 01:26:05 -0400

    Updates

---
+++ precache-manifest.caec75eac092dfebed66e96bb7462d70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b01592b38d71c9fdb6af4f9e27ac72",
+    "url": "/personalWebsite/index.html"
+  },
+  {
+    "revision": "03738c8d99a4f880c208",
+    "url": "/personalWebsite/static/css/main.099d7147.chunk.css"
+  },
+  {
+    "revision": "5f1f61af4f3d762df219",
+    "url": "/personalWebsite/static/js/2.7c1a5027.chunk.js"
+  },
+  {
+    "revision": "a531f1e27e5cd8d76c40d53140628768",
+    "url": "/personalWebsite/static/js/2.7c1a5027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03738c8d99a4f880c208",
+    "url": "/personalWebsite/static/js/main.38b72c3c.chunk.js"
+  },
+  {
+    "revision": "1d4f789ee7c66e1a6452",
+    "url": "/personalWebsite/static/js/runtime-main.9dded04c.js"
+  },
+  {
+    "revision": "85380af8aec3f2e0022598ab18581cc8",
+    "url": "/personalWebsite/static/media/bgcolor.85380af8.png"
+  },
+  {
+    "revision": "69f56014d7861c78fdfe94f01c32e9cf",
+    "url": "/personalWebsite/static/media/portrait.69f56014.jpg"
+  }
+]);
